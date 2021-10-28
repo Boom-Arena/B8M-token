9ff448a2c47a1cdb602447b87b20c8ef39e2c53a
German <germanllop@gmail.com>
2021-10-27 19:39:57 -0500

    pre deploy

--- contracts/BEP20.sol
+++ contracts/BEP20.sol
@@ -1,10 +1,10 @@
 // SPDX-License-Identifier: MIT
-pragma solidity >=0.4.22 <0.9.0;
+pragma solidity ^0.8.0;
 
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
 contract BEP20 is ERC20 {
-  constructor(uint256 initialSupply) ERC20("Boom Arena Mana","BAM") {
+  constructor(uint256 initialSupply) ERC20("Boom Arena","B8M") {
     _mint(msg.sender, initialSupply);
   }
 }
